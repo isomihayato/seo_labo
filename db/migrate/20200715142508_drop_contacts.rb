df911bf074b40aa63a582c5eef325263f01fdd7e
ice-crypto <testtest2tom@gmail.com>
2020-07-23 21:59:24 +0900

    本番環境対策

--- db/migrate/20200715142508_drop_contacts.rb
+++
@@ -1,5 +0,0 @@
-class DropContact < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :contacts
-  end
-end
--- db/migrate/20200715143727_drop_contact2.rb
+++
@@ -1,5 +0,0 @@
-class DropContact2 < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :contacts
-  end
-end
