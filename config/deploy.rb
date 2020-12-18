44f8a03c5f389465eab5816c113c6a5d4ddcbd45
石田朋也 <tomy@ishidatomoonarinoMacBook-Air.local>
2020-12-18 13:06:41 +0900

    +  master.key link

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,8 @@ set :deploy_to, '/var/www/seo_labo/'
 # シンボリックリンクをはるファイル
 set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
+
 # シンボリックリンクをはるフォルダ
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
